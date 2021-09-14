3b12cb1e9ea932672c7efa586ca49527076a1d75
Ashutosh Kumar <ashutosh.kumar3@conduent.com>
2021-09-14 19:39:30 +0530

    initial commit for reactive learnings

---
+++ src/main/java/com/techbuddy/reactive/ReactiveProgrammingLearningsApplication.java
@@ -0,0 +1,13 @@
+package com.techbuddy.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveProgrammingLearningsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveProgrammingLearningsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techbuddy/reactive/ReactiveProgrammingLearningsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbuddy.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveProgrammingLearningsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
