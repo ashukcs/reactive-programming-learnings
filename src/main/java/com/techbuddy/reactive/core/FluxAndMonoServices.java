647ec24c043f2284dc7ad7d49d01aa5853f8dc82
Ashutosh Kumar <ashutosh.kumar3@conduent.com>
2021-09-21 23:53:18 +0530

    commit changes for reactive core learning

--- src/main/java/com/techbuddy/reactive/core/FluxAndMonoServices.java
+++ src/main/java/com/techbuddy/reactive/core/FluxAndMonoServices.java
@@ -3,18 +3,77 @@ package com.techbuddy.reactive.core;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
 
 public class FluxAndMonoServices {
     public Flux<String> fruitsFlux(){
         return Flux.fromIterable(Arrays.asList("Apple", "Orange", "Banana")).log();
     }
 
+    public Flux<String> fruitsFluxMap(){
+        return Flux.fromIterable(Arrays.asList("Apple", "Orange", "Banana"))
+                .map(String::toUpperCase)
+                .log();
+    }
+
+    public Flux<String> fruitsFluxFilter(int number){
+        return Flux.fromIterable(Arrays.asList("Apple", "Orange", "Banana"))
+                .filter(s->s.length()>number)
+                .log();
+    }
+
+    public Flux<String> fruitsFluxFilterMap(int number){
+        return Flux.fromIterable(Arrays.asList("Apple", "Orange", "Banana"))
+                .filter(s->s.length()>number)
+                .map(String::toUpperCase)
+                .log();
+    }
+
     public Mono<String> fruitMono(){
         return Mono.just("Papaya").log();
     }
 
+    public Flux<String> fruitFluxFlatMapAsync(){
+        return Flux.fromIterable(Arrays.asList("Apple", "Orange", "Banana"))
+                .flatMap(s->Flux.just(s.split("")))
+                .delayElements(Duration.ofMillis(new Random().nextInt(1000)
+                )).log();
+    }
+
+    public Flux<String> fruitFluxConcatMap(){
+        return Flux.fromIterable(Arrays.asList("Apple", "Orange", "Banana"))
+                .concatMap(s->Flux.just(s.split("")))
+                .delayElements(Duration.ofMillis(new Random().nextInt(1000)
+                )).log();
+    }
+
+    public Mono<List<String>> fruitMonoFlatMap(){
+        return Mono.just("Papaya").flatMap(s->Mono.just(Arrays.asList(s.split("")))).log();
+    }
+
+    public Flux<String> fruitFluxFlatMapMany(){
+        return Mono.just("Apple")
+                .flatMapMany(s->Flux.just(s.split("")))
+                .log();
+    }
+
+    public Flux<String> fruitsFluxTransform(int number){
+        Function<Flux<String>,Flux<String>> filterData = data->data.filter(s->s.length()>number);
+        return Flux.fromIterable(Arrays.asList("Apple", "Orange", "Banana"))
+                .transform(filterData).log();
+    }
+
+    public Flux<String> fruitsFluxTransformDefaultIfEmpty(int number){
+        Function<Flux<String>,Flux<String>> filterData = data->data.filter(s->s.length()>number);
+        return Flux.fromIterable(Arrays.asList("Apple", "Orange", "Banana"))
+                .transform(filterData)
+                .defaultIfEmpty("default")
+                .log();
+    }
     public static void main(String[] args){
         FluxAndMonoServices fluxAndMonoServices = new FluxAndMonoServices();
         fluxAndMonoServices.fruitsFlux().subscribe(s -> System.out.println("Flux Item : " + s));
--- src/test/java/com/techbuddy/reactive/core/FluxAndMonoServicesTest.java
+++ src/test/java/com/techbuddy/reactive/core/FluxAndMonoServicesTest.java
@@ -1,13 +1,83 @@
 package com.techbuddy.reactive.core;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoServicesTest {
 
+    private FluxAndMonoServices fluxAndMonoServices = new FluxAndMonoServices();
     @Test
     void fruitsFlux() {
+        Flux<String> fruitsFlux = fluxAndMonoServices.fruitsFlux();
+        StepVerifier.create(fruitsFlux).expectNext("Apple", "Orange", "Banana").verifyComplete();
+    }
 
+    @Test
+    void fruitMono() {
+        Mono<String> fruitMono = fluxAndMonoServices.fruitMono();
+        StepVerifier.create(fruitMono).expectNext("Papaya").verifyComplete();
+    }
+
+    @Test
+    void fruitsFluxMap() {
+        Flux<String> fruitsFlux = fluxAndMonoServices.fruitsFluxMap();
+
+        StepVerifier.create(fruitsFlux).expectNext("APPLE", "ORANGE", "BANANA").verifyComplete();
+
+    }
+
+    @Test
+    void fruitsFluxFilter() {
+        Flux<String> fruitsFluxFilter = fluxAndMonoServices.fruitsFluxFilter(5);
+        StepVerifier.create(fruitsFluxFilter).expectNext("Orange", "Banana").verifyComplete();
+    }
+
+    @Test
+    void fruitsFluxFilterMap() {
+        Flux<String> fruitsFluxFilterMap = fluxAndMonoServices.fruitsFluxFilterMap(5);
+        StepVerifier.create(fruitsFluxFilterMap).expectNext("ORANGE", "BANANA").verifyComplete();
+    }
+
+    @Test
+    void fruitMonoFlatMap() {
+        Mono<List<String>> listMono = fluxAndMonoServices.fruitMonoFlatMap();
+        StepVerifier.create(listMono).expectNextCount(1).verifyComplete();
+    }
+
+    @Test
+    void fruitFluxFlatMapAsync() {
+        Flux<String> listFlux = fluxAndMonoServices.fruitFluxFlatMapAsync();
+        StepVerifier.create(listFlux).expectNextCount(17).verifyComplete();
+    }
+
+    @Test
+    void fruitFluxConcatMap() {
+        Flux<String> listFlux = fluxAndMonoServices.fruitFluxConcatMap();
+        StepVerifier.create(listFlux).expectNextCount(17).verifyComplete();
+    }
+
+    @Test
+    void fruitFluxFlatMapMany() {
+        Flux<String> listStringFlux = fluxAndMonoServices.fruitFluxFlatMapMany();
+        StepVerifier.create(listStringFlux).expectNextCount(5).verifyComplete();
+    }
+
+    @Test
+    void fruitsFluxTransform() {
+        Flux<String> transform = fluxAndMonoServices.fruitsFluxTransform(5);
+        StepVerifier.create(transform).expectNext("Orange","Banana").verifyComplete();
+    }
+
+    @Test
+    void fruitsFluxTransformDefaultIfEmpty() {
+        Flux<String> transform = fluxAndMonoServices.fruitsFluxTransformDefaultIfEmpty(10);
+        StepVerifier.create(transform).expectNext("default").verifyComplete();
     }
 }
